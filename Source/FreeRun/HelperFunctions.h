5e43f7c2f5e2faf0e73d078eee381e83fcf9dff8
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-07-30 23:55:25 +0300

    add some functions to help vector, rotator calculations

--- Source/FreeRun/HelperFunctions.h
+++ Source/FreeRun/HelperFunctions.h
@@ -1,48 +1,51 @@
-﻿#include "Kismet/KismetMathLibrary.h"
+﻿#pragma once
+#include "Kismet/KismetMathLibrary.h"
 
-FVector MoveUp(FVector Source, float UpValue)
+namespace HelperFunc
 {
-	return Source + FVector(0,0, UpValue);
-}
+	inline FVector MoveUp(FVector Source, float UpValue)
+	{
+		return Source + FVector(0,0, UpValue);
+	}
 
-FVector MoveDown(FVector Source, float DownValue)
-{
-	return Source - FVector(0,0, DownValue);
-}
+	inline FVector MoveDown(FVector Source, float DownValue)
+	{
+		return Source - FVector(0,0, DownValue);
+	}
 
-FVector MoveRight(FVector Source, float RightValue, FRotator Rotator)
-{
-	FVector RightVector = Rotator.Vector().RightVector;
-	return Source + (RightVector * RightValue);
-}
+	inline FVector MoveRight(FVector Source, float RightValue, FRotator Rotator)
+	{
+		FVector RightVector = Rotator.Vector().RightVector;
+		return Source + (RightVector * RightValue);
+	}
 
-FVector MoveLeft(FVector Source, float LeftValue, FRotator Rotator)
-{
-	FVector LeftVector = -Rotator.Vector().RightVector;
-	return Source + (LeftVector * LeftValue);
-}
+	inline FVector MoveLeft(FVector Source, float LeftValue, FRotator Rotator)
+	{
+		FVector LeftVector = -Rotator.Vector().RightVector;
+		return Source + (LeftVector * LeftValue);
+	}
+	
+	inline FVector MoveForward(FVector Source, float ForwardValue, FRotator Rotator)
+	{
+		FVector ForwardVector = UKismetMathLibrary::GetForwardVector(Rotator);
+		return Source + (ForwardVector * ForwardValue);
+	}
 
+	inline FVector MoveBackward(FVector Source, float BackwardValue, FRotator Rotator)
+	{
+		FVector BackwardVector = -UKismetMathLibrary::GetForwardVector(Rotator);
+		return Source + (BackwardVector * BackwardValue);
+	}
 
-FVector MoveForward(FVector Source, float ForwardValue, FRotator Rotator)
-{
-	FVector ForwardVector = UKismetMathLibrary::GetForwardVector(Rotator);
-	return Source + (ForwardVector * ForwardValue);
-}
+	inline FRotator ReverseNormal(FVector Normal)
+	{
+		FRotator RotFromX = UKismetMathLibrary::MakeRotFromX(Normal);
+		FRotator DeltaRotator = RotFromX - FRotator(0, 180.f, 0);
+		return FRotator(0, DeltaRotator.Yaw, 0);
+	}
 
-FVector MoveBackward(FVector Source, float BackwardValue, FRotator Rotator)
-{
-	FVector BackwardVector = -UKismetMathLibrary::GetForwardVector(Rotator);
-	return Source + (BackwardVector * BackwardValue);
-}
-
-FRotator ReverseNormal(FVector Normal)
-{
-	FRotator RotFromX = UKismetMathLibrary::MakeRotFromX(Normal);
-	FRotator DeltaRotator = RotFromX - FRotator(0, 180.f, 0);
-	return FRotator(0, DeltaRotator.Yaw, 0);
-}
-
-FRotator AddRotator(FRotator InitialRotator, FRotator AdderRotator)
-{
-	return InitialRotator + AdderRotator;
+	inline FRotator AddRotator(FRotator InitialRotator, FRotator AdderRotator)
+	{
+		return InitialRotator + AdderRotator;
+	}
 }
