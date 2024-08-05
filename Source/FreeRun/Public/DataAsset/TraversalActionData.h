ec73a120a19812d096227bec7c7bcae67ad60dc2
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-08-05 23:28:49 +0300

    create data asset class to store parkour anim requirements

--- Source/FreeRun/Public/DataAsset/TraversalActionData.h
+++ Source/FreeRun/Public/DataAsset/TraversalActionData.h
@@ -3,15 +3,32 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "FreeRun/Public/Enums/TraversalEnum.h"
 #include "Engine/DataAsset.h"
 #include "TraversalActionData.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class FREERUN_API UTraversalActionData : public UPrimaryDataAsset
 {
 	GENERATED_BODY()
-	
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Data)
+	UAnimMontage* ActionMontage;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Data)
+	float Warp1XOffset;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Data)
+	float Warp1ZOffset;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Data)
+	float Warp2XOffset;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Data)
+	float Warp2ZOffset;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Data)
+	float Warp3XOffset;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Data)
+	float Warp3ZOffset;
+	UPROPERTY(EditAnywhere, Category=Data)
+	ETraversalState InState;  // Current Traversal State
+	UPROPERTY(EditAnywhere, Category=Data)
+	ETraversalState OutState; // Next Traversal State
 };
